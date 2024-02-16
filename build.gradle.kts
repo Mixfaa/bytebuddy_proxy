b43fddf0124eb2f805d931f2a71a0d9f224ca2d0
Миша Език <mishunder9354@gmail.com>
2024-02-16 20:51:43 +0200

    hello_jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 group = "com.github.Mixfaa"
-version = "1.1-SNAPSHOT"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("net.bytebuddy:byte-buddy:1.14.12+")
+    implementation("net.bytebuddy:byte-buddy:1.14.12")
 }
 
 java {
