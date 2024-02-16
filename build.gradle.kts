da6fd16f1f612cd682eddc966b4b4538c1b8cc35
Миша Език <mishunder9354@gmail.com>
2024-02-16 20:30:21 +0200

    _

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("java")
-    id("maven-publish")
 }
 
 group = "com.mixfa"
-version = "1.3-SNAPSHOT"
+version = "1.4-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -13,23 +12,3 @@ repositories {
 dependencies {
     implementation("net.bytebuddy:byte-buddy:1.14.12+")
 }
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Mixfaa/bytebuddy_proxy")
-            credentials {
-                username = project.findProperty("github.username") as String
-                password = project.findProperty("github.token") as String
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
-}
-
-
-
